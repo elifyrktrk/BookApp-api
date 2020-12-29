3b30f1f5eafea1245a046127187f3676c6d37ea7
elif yürektürk <36786466+elifyrktrk@users.noreply.github.com>
2020-12-29 22:00:24 +0300

    category routerı tamamlandı

--- router/book.js
+++ router/book.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const Book = require("../model/Book");
+const Category = require("../model/Category");
 
 router.get("/", (req,res) => {
     Book.aggregate([
-        
         {   
             $lookup: {
                 from: 'categories',
@@ -60,6 +60,14 @@ router.get("/detail/:id", (req,res) => {
 router.post("/add", (req,res) => {
    let new_book = new Book(req.body);
    new_book.save();
+    new_book.category.forEach(category_id => {
+        Category.findOne({_id: category_id}, (err, data) => {
+            if(!err){
+                data.books.push(new_book._id);
+                data.save();
+            }
+        });
+    });
    res.json({
        success: true,
        message: "İşlem başarılı",
